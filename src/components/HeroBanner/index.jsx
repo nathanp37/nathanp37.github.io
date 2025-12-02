72d5d8bdc9dd8a5c6e5c042d219eafefeb5f0deb
Estebancott <estebancott@gmail.com>
2025-12-02 16:39:57 +0100

    un peu mieux

--- src/components/HeroBanner/index.jsx
+++ src/components/HeroBanner/index.jsx
@@ -4,7 +4,7 @@ const HeroBanner = () => {
     return (
         <div className={styles.container}>
             <div className={styles.description}>
-                <p className={styles.title}> Nathan <span>PRUNOTTO</span></p>
+                <p className={styles.title}> Nathan <br></br><span>PRUNOTTO</span></p>
                 <p className={styles.descriptionOfDescription}>
                     Jeune diplômé de l’ENSIM, spécialisé en acoustique et vibrations, passionné par l’innovation et la recherche.
                     Candidat à une thèse ou à un poste d’ingénieur en traitement du signal, imagerie, vibrations ou intelligence artificielle.
@@ -12,11 +12,10 @@ const HeroBanner = () => {
             </div>
             <div className={styles.containerSkills}>
                 <div className={styles.skills}>
-                    <div className={`${styles.skill} ${styles.cpp}`}>Python</div>
-                    <div className={`${styles.skill} ${styles.c}`}>Matlab</div>
-                    <div className={`${styles.skill} ${styles.python}`}>Solidworks</div>
+                    <div className={`${styles.skill} ${styles.cpp}`}>Matlab</div>
+                    <div className={`${styles.skill} ${styles.c}`}>Solidworks</div>
+                    <div className={`${styles.skill} ${styles.python}`}>Python</div>
                     <div className={`${styles.skill} ${styles.bash}`}>Kicad</div>
-                    <div className={`${styles.skill} ${styles.bash}`}>Comsol multiphysics</div>
                 </div>
             </div>
             <div className={styles.banner}>
