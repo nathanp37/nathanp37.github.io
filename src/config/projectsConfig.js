8f7cd67bc6c5fbb6d9532b0d7bbb152785785994
Estebancott <estebancott@gmail.com>
2025-12-21 12:53:35 +0100

    mobile navbar fix + package update

--- src/config/projectsConfig.js
+++ src/config/projectsConfig.js
@@ -1,11 +1,2 @@
 // Ce fichier est auto-généré. Ne pas modifier manuellement.
-export const PROJECTS_LIST = [
-    "Analyse_Modale",
-    "Bilan_Carbone",
-    "Isolation_Porte",
-    "Projet5A_Mobius",
-    "StageCEA",
-    "Vibroacoustique",
-    "Projet4A_Robotique",
-    "Caracterisation_Acoustique"
-];
+export const PROJECTS_LIST = [];
