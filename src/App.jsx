f0e3bc4c230ef31e1d8b94643c5ce4a864cd34e8
nathanp37 <nathanp37190@gmail.com>
2025-12-22 15:27:04 +0100

    avion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"; // Ne pas oublier d'importer useEffect
+import { useEffect } from "react";
 import styles from "./styles/main.module.scss";
 import Header from "./components/Header/index.jsx";
 import HeroBanner from "./components/HeroBanner/index.jsx";
@@ -9,17 +9,17 @@ const App = () => {
     const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1451230225760522405/kja_Rq2V0HYM6C7mqptqkpiNmcT09BX4rLZrTN6g0_jWK7O3lJkdRPz7AEDNtlF7a0LM';
 
     const sendNotification = async () => {
-        // Récupère la date et l'heure actuelle
+     
         const now = new Date();
-        const dateString = now.toLocaleDateString('fr-FR'); // ex: 18/12/2024
-        const timeString = now.toLocaleTimeString('fr-FR'); // ex: 16:30:05
+        const dateString = now.toLocaleDateString('fr-FR');
+        const timeString = now.toLocaleTimeString('fr-FR');
 
         try {
             await fetch(DISCORD_WEBHOOK_URL, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    content: `🚀 Nouvelle visite sur ton portfolio !\n📅 **Date** : ${dateString}\n⏰ **Heure** : ${timeString}`,
+                    content: `🚀 Nouvelle visite sur ton portfolio !\n📅 Date : ${dateString}\n⏰ Heure : ${timeString}`,
                     username: "Portfolio Watcher"
                 }),
             });
--- src/config/projectsConfig.js
+++ src/config/projectsConfig.js
@@ -1,2 +1,12 @@
 // Ce fichier est auto-généré. Ne pas modifier manuellement.
-export const PROJECTS_LIST = [];
+export const PROJECTS_LIST = [
+    "Analyse_Modale",
+    "Bilan_Carbone",
+    "Isolation_Porte",
+    "Projet5A_Mobius",
+    "StageCEA",
+    "Vibroacoustique",
+    "Projet4A_Robotique",
+    "Caracterisation_Acoustique",
+    "Aeroelasticite_Aile"
+];
